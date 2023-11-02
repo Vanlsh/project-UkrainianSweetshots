6cdaec84fcf13cec8baf8cb4b3c5688200a641e0
Vanlsh <92462786+Vanlsh@users.noreply.github.com>
2023-11-02 20:09:43 +0200

    fix js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const elementToToggle = document.querySelector('.overlay');
 const toggleButton = document.querySelector('#open-menu');
 const closeButton = document.querySelector('#close-menu');
 const headerMobMenu = document.querySelector('.header-mob-menu');
+const linksMenu = document.querySelectorAll('.header-menu-link');
 function toggleClass() {
   elementToToggle.classList.toggle('is-open');
 }
@@ -9,3 +10,7 @@ closeButton.addEventListener('click', toggleClass);
 toggleButton.addEventListener('click', toggleClass);
 elementToToggle.addEventListener('click', toggleClass);
 headerMobMenu.addEventListener('click', e => e.stopImmediatePropagation());
+
+for (let i = 0; i < linksMenu.length; i++) {
+  linksMenu[i].addEventListener('click', toggleClass);
+}
